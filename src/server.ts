5cd3cfd0c435d6b6b5918c94eaeb8513477e216a
Rafael Moura Friederick <rafael.mf.documents@gmail.com>
2023-05-16 02:11:32 -0300

    feat: Set configuration to rocketseat eslint config

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,16 @@
-import fastify from 'fastify';
+import fastify from 'fastify'
 
-const PORT = 3333;
-const app = fastify();
+const PORT = 3333
+const app = fastify()
 
-app.listen({
-  port: PORT,
-}).then(() => {
-  console.log(`Listening at port ${PORT}`)
+app.post('/hello', () => {
+  return 'Hello World'
 })
+
+app
+  .listen({
+    port: PORT,
+  })
+  .then(() => {
+    console.log(`Listening at port ${PORT}`)
+  })
