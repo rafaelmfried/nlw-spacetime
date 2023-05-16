cb186664f7f6d0fd52fe92547b4ceb4b15bb4074
Rafael Moura Friederick <rafael.mf.documents@gmail.com>
2023-05-16 02:43:23 -0300

    feat: install prisma and config a user model and prisma client

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,17 @@
 import fastify from 'fastify'
+import dotenv from 'dotenv'
+import { PrismaClient } from '@prisma/client'
 
-const PORT = 3333
+dotenv.config()
+
+const PORT: number = parseInt(process.env.PORT || '3333')
+
 const app = fastify()
+const prisma = new PrismaClient()
 
-app.post('/hello', () => {
-  return 'Hello World'
+app.post('/users', async () => {
+  const users = await prisma.user.findMany()
+  return users
 })
 
 app
